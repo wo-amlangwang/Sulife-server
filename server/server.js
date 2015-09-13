cf3284aa4b60ceb1c71199dc4eb5f8825523c8e7
wo-amlangwang <adamlangwang@gmail.com>
2015-09-13 00:13:36 -0400

    set up II

--- server/server.js
+++ server/server.js
@@ -1,13 +1,39 @@
 var express = require('express');
 var app = express();
+//used to connected mongodb
 var mongoose = require('mongoose');
+//express middlewares
 var flash = require('connect-flash');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var session = require('express-session');
+var session = require('client-sessions');
+//require main passport
 var passport = require('passport');
+//connect to mongo db
+var mongoURL = require('./mongo_modules/mongoURL.json').url;
+mongoose.connect(mongoURL,function(err) {
+  if(err) {
+    console.log(err);
+    process.exit();
+  } else {
+    console.log('Connect to mongodb');
+  }
+});
+//use middlewares for every route
 
-
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(session({'cookieName' : 'session',
+                 'secret' : 'i am a zombie, but i drink milk 0W0',
+                 'duration' : 12 * 60 * 60 * 1000,
+                 'activeDuration' : 5 * 60 * 1000}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+//give app and passport router
+var router = require('./router.js');
+router(app,passport);
+//listen on port
 var port = process.env.PORT || 4000;
 app.listen(port,function(err) {
   if(err) {
