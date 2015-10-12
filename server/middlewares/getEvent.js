8941866a0bf6af966f81d5e878bdd7d596d212f7
wo-amlangwang <adamlangwang@gmail.com>
2015-10-12 00:04:05 -0400

    fix bugs
    can’t do two send for same request,
    so I move second send into json

--- server/middlewares/getEvent.js
+++ server/middlewares/getEvent.js
@@ -3,8 +3,8 @@ module.exports = {
   getEvent : function(req, res, next) {
     Event.findOne({'id' : req.params.eventid}, function(err, thisevent) {
       if(req.userid === thisevent.userid){
-        res.status(200).send({'message' : 'OK!'});
-        res.send(thisevent);
+        res.status(200).send({'message' : 'OK!',
+                              'event' : thisevent});
       } else {
         res.status(403).send({'message' : 'You have no right to do this'});
       }
