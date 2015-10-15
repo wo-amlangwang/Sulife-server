6414eae81a5cd3326dcebd7173e40b77e49fc473
wo-amlangwang <adamlangwang@gmail.com>
2015-10-14 20:18:32 -0400

    fix bugs

--- server/middlewares/getProfile.js
+++ server/middlewares/getProfile.js
@@ -2,20 +2,27 @@ var Profile = require('../mongo_modules/profile.js');
 module.exports = {
   getProfile : function(req,res,next) {
     Profile.findOne({'userid' : req.userid},function(err,profile) {
-      if(!profile){
-        var newProfile = new Profile();
-        newProfile.userid = req.userid;
-        newProfile.firstname = null;
-        newProfile.lastname = null;
-        newProfile.save(function(err,thisprofile) {
+      if(err){
+        req.reJson['message'] = 'something wrong when get data from database';
+        req.reJson['err'] = err;
+        res.status(500).send(req.reJson);
+      }
+      else {
+        if(!profile){
+          var newProfile = new Profile();
+          newProfile.userid = req.userid;
+          newProfile.firstname = null;
+          newProfile.lastname = null;
+          newProfile.save(function(err,thisprofile) {
+            req.reJson['message'] = 'OK! Profile followed';
+            req.reJson['profile'] = thisprofile;
+            res.status(200).send(req.reJson);
+          });
+        }else {
           req.reJson['message'] = 'OK! Profile followed';
-          req.reJson['profile'] = thisprofile;
+          req.reJson['profile'] = profile;
           res.status(200).send(req.reJson);
-        });
-      }else {
-        req.reJson['message'] = 'OK! Profile followed';
-        req.reJson['profile'] = profile;
-        res.status(200).send(req.reJson);
+        }
       }
     });
   }
