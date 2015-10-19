115ad520deb8d7d5aba650fae0c6da6820f05e5b
wo-amlangwang <adamlangwang@gmail.com>
2015-10-18 22:38:07 -0400

    revent

--- FrontEnd/SwiftLoginScreen/SwiftLoginScreen/AppDelegate.swift
+++ FrontEnd/SwiftLoginScreen/SwiftLoginScreen/AppDelegate.swift
@@ -1,22 +1,15 @@
-//
-//  AppDelegate.swift
-//  SuLife
-//
-//  Created by Sine Feng on 9/14/15.
-//  Copyright (c) 2015 Sine Feng. All rights reserved.
-//
+
 
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+                            
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        return true;
     }
 
     func applicationWillResignActive(application: UIApplication) {
--- FrontEnd/SwiftLoginScreen/SwiftLoginScreen/LoginVC.swift
+++ FrontEnd/SwiftLoginScreen/SwiftLoginScreen/LoginVC.swift
@@ -1,88 +1,84 @@
-//
-//  LoginVC.swift
-//  SuLife
-//
-//  Created by Sine Feng on 10/12/15.
-//  Copyright © 2015 Sine Feng. All rights reserved.
+
 //
 
 import UIKit
 
-class LoginVC: UIViewController {
+class LoginVC: UIViewController,UITextFieldDelegate {
     
-    @IBOutlet weak var userEmailTextField: UITextField!
-    @IBOutlet weak var userPasswordTextField: UITextField!
+    
+    @IBOutlet var txtUsername : UITextField!
+    @IBOutlet var txtPassword : UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
+    // #pragma mark - Navigation
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
     }
     */
     
-    @IBAction func loginButtonTapped(sender: AnyObject) {
+    @IBAction func signinTapped(sender : UIButton) {
+        let username:NSString = txtUsername.text!
+        let password:NSString = txtPassword.text!
         
-        // TODO: from server
-        let userEmail = userEmailTextField.text!
-        let userPassword = userPasswordTextField.text!
-        
-        // fill in required fields
-        if ( userEmail.isEmpty )
-        {
+        if ( username.isEqualToString("") || password.isEqualToString("") ) {
+            
             let alertView:UIAlertView = UIAlertView()
-            alertView.title = "Log in Failed!"
-            alertView.message = "Please enter Email"
+            alertView.title = "Sign in Failed!"
+            alertView.message = "Please enter Username and Password"
             alertView.delegate = self
             alertView.addButtonWithTitle("OK")
             alertView.show()
-        }
-        else if ( userPassword.isEmpty )
-        {
-            let alertView:UIAlertView = UIAlertView()
-            alertView.title = "Log in Failed!"
-            alertView.message = "Please enter Password"
-            alertView.delegate = self
-            alertView.addButtonWithTitle("OK")
-        }
-        else
-        {
+        } else {
+            
             do {
-                let post:NSString = "email=\(userEmail)&password=\(userPassword)"
+            let post:NSString = "username=\(username)&password=\(password)"
+            
+            NSLog("PostData: %@",post);
+            
+            let url:NSURL = NSURL(string:"https://dipinkrishna.com/jsonlogin2.php")!
+            
+            let postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
+            
+            let postLength:NSString = String( postData.length )
+            
+            let request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            request.HTTPBody = postData
+            request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            
+            var reponseError: NSError?
+            var response: NSURLResponse?
+            
+            var urlData: NSData?
+            do {
+                urlData = try NSURLConnection.sendSynchronousRequest(request, returningResponse:&response)
+            } catch let error as NSError {
+                reponseError = error
+                urlData = nil
+            }
+            
+            if ( urlData != nil ) {
+                let res = response as! NSHTTPURLResponse!;
                 
-                NSLog("PostData: %@",post);
-                
-                let url:NSURL = NSURL(string:"https://damp-retreat-5682.herokuapp.com/local/login")!
-                
-                let postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
-                
-                let postLength:NSString = String( postData.length )
-                
-                let request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
-                request.HTTPMethod = "POST"
-                request.HTTPBody = postData
-                request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
-                request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-                request.setValue("application/json", forHTTPHeaderField: "Accept")
-                
-                
-                var reponseError: NSError?
-                var response: NSURLResponse?
-                
+<<<<<<< HEAD
                 var urlData: NSData?
                 do {
                     urlData = try NSURLConnection.sendSynchronousRequest(request, returningResponse:&response)
@@ -90,76 +86,73 @@ class LoginVC: UIViewController {
                     reponseError = error
                     urlData = nil
                 }
+=======
+                NSLog("Response code: %ld", res.statusCode);
+>>>>>>> parent of 08e21a6... New Login and Contacts
                 
-                if ( urlData != nil )
+                if (res.statusCode >= 200 && res.statusCode < 300)
                 {
-                    let res = response as! NSHTTPURLResponse!;
-                    if (res != nil)
+                    let responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+                    
+                    NSLog("Response ==> %@", responseData);
+                    
+                    //var error: NSError?
+                    
+                    let jsonData:NSDictionary = try NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
+                    
+                    
+                    let success:NSInteger = jsonData.valueForKey("success") as! NSInteger
+                    
+                    //[jsonData[@"success"] integerValue];
+                    
+                    NSLog("Success: %ld", success);
+                    
+                    if(success == 1)
                     {
-                        NSLog("Response code: %ld", res.statusCode);
+                        NSLog("Login SUCCESS");
                         
-                        if (res.statusCode >= 200 && res.statusCode < 300)
-                        {
-                            let responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
-                            
-                            NSLog("Response ==> %@", responseData);
-                            
-                            //var error: NSError?
-                            
-                            let jsonData:NSDictionary = try NSJSONSerialization.JSONObjectWithData(urlData!, options:NSJSONReadingOptions.MutableContainers ) as! NSDictionary
-                            
-                            
-                            let success:NSString = jsonData.valueForKey("message") as! NSString
-                            
-                            //[jsonData[@"success"] integerValue];
-                            
-                            NSLog("Success: %@", success);
-                            
-                            if(success == "OK")
-                            {
-                                NSLog("Login SUCCESS");
-                                
-                                let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
-                                prefs.setObject(userEmail, forKey: "Email")
-                                prefs.setInteger(1, forKey: "isUserLoggedIn")
-                                prefs.synchronize()
-                                
-                                self.dismissViewControllerAnimated(true, completion: nil)
-                            } else {
-                                var error_msg:NSString
-                                
-                                if jsonData["error_message"] as? NSString != nil {
-                                    error_msg = jsonData["error_message"] as! NSString
-                                } else {
-                                    error_msg = "Unknown Error"
-                                }
-                                let alertView:UIAlertView = UIAlertView()
-                                alertView.title = "Sign in Failed!"
-                                alertView.message = error_msg as String
-                                alertView.delegate = self
-                                alertView.addButtonWithTitle("OK")
-                                alertView.show()
-                            }
-                        }
+                        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+                        prefs.setObject(username, forKey: "USERNAME")
+                        prefs.setInteger(1, forKey: "ISLOGGEDIN")
+                        prefs.synchronize()
+                        
+                        self.dismissViewControllerAnimated(true, completion: nil)
                     } else {
+                        var error_msg:NSString
+                        
+                        if jsonData["error_message"] as? NSString != nil {
+                            error_msg = jsonData["error_message"] as! NSString
+                        } else {
+                            error_msg = "Unknown Error"
+                        }
                         let alertView:UIAlertView = UIAlertView()
                         alertView.title = "Sign in Failed!"
-                        alertView.message = "Please check your Email and Password!\nIf you haven't registered,\ntry register first!"
+                        alertView.message = error_msg as String
                         alertView.delegate = self
                         alertView.addButtonWithTitle("OK")
                         alertView.show()
+                        
                     }
+                    
                 } else {
                     let alertView:UIAlertView = UIAlertView()
                     alertView.title = "Sign in Failed!"
-                    alertView.message = "Connection fail!"
-                    if let error = reponseError {
-                        alertView.message = (error.localizedDescription)
-                    }
+                    alertView.message = "Connection Failed"
                     alertView.delegate = self
                     alertView.addButtonWithTitle("OK")
                     alertView.show()
                 }
+            } else {
+                let alertView:UIAlertView = UIAlertView()
+                alertView.title = "Sign in Failed!"
+                alertView.message = "Connection Failure"
+                if let error = reponseError {
+                    alertView.message = (error.localizedDescription)
+                }
+                alertView.delegate = self
+                alertView.addButtonWithTitle("OK")
+                alertView.show()
+            }
             } catch {
                 let alertView:UIAlertView = UIAlertView()
                 alertView.title = "Sign in Failed!"
@@ -169,6 +162,7 @@ class LoginVC: UIViewController {
                 alertView.show()
             }
         }
+        
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {   //delegate method
@@ -176,4 +170,3 @@ class LoginVC: UIViewController {
         return true
     }
 }
-
