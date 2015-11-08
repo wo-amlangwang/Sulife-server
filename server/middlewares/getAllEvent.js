4370dbb46eb9aefdb6bf30a2fb371b6073abe597
wo-amlangwang <adamlangwang@gmail.com>
2015-11-08 18:31:54 -0500

    fix bugs

--- server/middlewares/getAllEvent.js
+++ server/middlewares/getAllEvent.js
@@ -15,17 +15,24 @@ module.exports = {
     });
   },
   getEventByDate : function(req,res,next) {
-    Event.find({'userid' : req.userid , 'endtime' : {'$gte' : req.body.starttime}})
-    .exec(function(err,events) {
-      if(err){
-        req.reJson['message'] = 'something wrong when get data from database';
-        req.reJson['err'] = err;
-        res.status(500).send(req.reJson);
-      }else {
-        req.reJson['message'] = 'OK! Events list followed';
-        req.reJson['Events'] = events;
-        res.status(200).send(req.reJson);
-      }
-    });
+    if(req.body.endtime  < req.body.starttime){
+      req.reJson['message'] = 'starttime greater than endtime';
+      res.status(400).send(req.reJson);
+    }else {
+      Event.find({'userid' : req.userid ,
+                  'starttime' : {'$lt' : req.body.endtime} ,
+                  'endtime' : {'$gte' : req.body.starttime}})
+      .exec(function(err,events) {
+        if(err){
+          req.reJson['message'] = 'something wrong when get data from database';
+          req.reJson['err'] = err;
+          res.status(500).send(req.reJson);
+        }else {
+          req.reJson['message'] = 'OK! Events list followed';
+          req.reJson['Events'] = events;
+          res.status(200).send(req.reJson);
+        }
+      });
+    }
   }
 }
