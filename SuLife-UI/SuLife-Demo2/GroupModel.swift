39c141fbb57c3610b5a3da82b1b1ac423a3a66d3
Sine Feng <sine@SineFeng.local>
2015-11-11 19:37:14 -0500

    Classes Created

---
+++ SuLife-UI/SuLife-Demo2/GroupModel.swift
@@ -0,0 +1,13 @@
+//
+//  GroupModel.swift
+//  SuLife-Demo2
+//
+//  Created by Sine Feng on 11/11/15.
+//  Copyright © 2015 Sine Feng. All rights reserved.
+//
+
+import UIKit
+
+class GroupModel: NSObject {
+
+}
