883f4ea491fad3f89a5cab6e5cdea8e787466c10
wo-amlangwang <adamlangwang@gmail.com>
2015-11-13 00:31:55 -0500

    update getfriends

--- server/middlewares/getFriends.js
+++ server/middlewares/getFriends.js
@@ -8,9 +8,20 @@ module.exports = {
         res.status(500).send(req.reJson);
       }else {
         req.reJson['message'] = 'OK! relationships followed';
-        req.reJson['relationships'] = relationships;
-        res.status(200).send(req.reJson);
+        req.reJson['relationships'][0] = relationships;
+        Friendlist.find({'userid2' : req.userid},function(err,relationships) {
+          if(err){
+            req.reJson['message'] = 'something wrong when get data from database';
+            req.reJson['err'] = err;
+            res.status(500).send(req.reJson);
+          }else {
+            req.reJson['message'] = 'OK! relationships followed';
+            req.reJson['relationships'][1] = relationships;
+            res.status(200).send(req.reJson);
+          }
+        });
+        //res.status(200).send(req.reJson);
       }
-    })
+    });
   }
 }
