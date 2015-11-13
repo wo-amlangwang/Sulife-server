1665d34f1f85b5d1f98c44144a0d9712e2381948
wo-amlangwang <adamlangwang@gmail.com>
2015-11-13 10:39:40 -0500

    make better

--- server/middlewares/getFriends.js
+++ server/middlewares/getFriends.js
@@ -8,20 +8,8 @@ module.exports = {
         res.status(500).send(req.reJson);
       }else {
         req.reJson['message'] = 'OK! relationships followed';
-        req.reJson['relationships'] = [];
-        req.reJson['relationships'].push(relationships);
-        Friendlist.find({'userid2' : req.userid},function(err,relationships) {
-          if(err){
-            req.reJson['message'] = 'something wrong when get data from database';
-            req.reJson['err'] = err;
-            res.status(500).send(req.reJson);
-          }else {
-            req.reJson['message'] = 'OK! relationships followed';
-            req.reJson['relationships'].push(relationships);
-            res.status(200).send(req.reJson);
-          }
-        });
-        //res.status(200).send(req.reJson);
+        req.reJson['relationships'] = relationships;
+        res.status(200).send(req.reJson);
       }
     });
   }
