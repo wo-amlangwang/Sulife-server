629e3da5f1c9f617c45b590a32a623d7bc94c463
wo-amlangwang <adamlangwang@gmail.com>
2015-11-14 07:00:37 -0500

    new schema to keep location
    new schema to keep location

---
+++ server/mongo_modules/location.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var LocationSchema = mongoose.Schema({
+  userid : String,
+  EventOrTaskid : String,
+  location : {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [Number]
+  }
+});
+
+LocationSchema.index({ location : '2dsphere' });
+
+module.exports = mongoose.model('Location', LocationSchema);
