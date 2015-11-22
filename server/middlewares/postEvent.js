516a2b3ed8a7005ee21c3590d93cc9c1ccdd7054
wo-amlangwang <adamlangwang@gmail.com>
2015-11-22 14:18:19 -0500

    fix bugs

--- server/middlewares/postEvent.js
+++ server/middlewares/postEvent.js
@@ -8,13 +8,14 @@ module.exports = {
     newEvent.starttime = req.body.starttime;
     newEvent.endtime = req.body.endtime;
     newEvent.share = req.body.share;
-    newEvent.save(function(err) {
+    newEvent.save(function(err,thisevent) {
       if(err){
         req.reJson['message'] = 'something wrong when put into database';
         req.reJson['err'] = err;
         res.status(500).send(req.reJson);
       }else {
         req.reJson['message'] = 'OK!';
+        req.reJson['event'] = thisevent;
         res.status(200).send(req.reJson);
       }
     });
